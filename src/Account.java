00c4e5403122629fd4cdc5a0e11adebe29a3ba34
user <gulira.abdukerimkyzy.jk@gmail.com>
2023-02-02 23:46:42 +0600

    addAccount

--- src/Account.java
+++ src/Account.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Account {
+public class Account {
+    double balance = 1000;
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdrawal(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+
+        } else {
+            System.out.println("Недостоточно средств: ");
+        }
+
+    }
+
+    public double getBalance() {
+        return balance;
+    }
 }
+
+
+/*Account деген класс түзүңүз, анын сөзсүз double balance = 1000; деген полясы болсун
+жана сиз каалагандай поля кошсоңуз болот.
+deposit() - баланска акча кошот,
+withdrawal() - баланстан акча алат
+методдору болсун.
+main методдон 2-3 account түзүп ар кандай операцияларды жасаңыз.*/
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Account account1 = new Account();
+        Account account2 = new Account();
+        Account account3 = new Account();
+
+        Scanner scanner = new Scanner(System.in);
+        account1.deposit(scanner.nextDouble());
+        account2.withdrawal(scanner.nextDouble());
+
+        System.out.println("Депозит жана балансыныздагы сумма биригип: " + account1.getBalance());
+        System.out.println("Сиздин азыркы учурдагы акчаныз : " + account2.getBalance());
+        System.out.println("Сиздин балансынызда  " + account3.getBalance() + " сом болгон");
+
+
     }
+
+
 }
